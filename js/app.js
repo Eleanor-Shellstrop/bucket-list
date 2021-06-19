66f5ac616b33ab98811abbc76897c1630faf9673
Anne Ensign <anne.ensign@gmail.com>
2021-06-19 15:49:41 -0400

    Add select menu, test with some small arrays

--- js/app.js
+++ js/app.js
@@ -1,13 +1,57 @@
-const summerList = [
-    "Water balloon fight",
-    "Picnic in the Park",
-    "Pool Day",
-    "U-Pick Produce at a Farm"
+const selection = document.getElementById('seasonSelect');
+
+const xmasList = [
+    "Make a Gingerbread House",
+    "U-Pick Christmas Tree",
+    "Donate Presents to a Sponsored Family",
+    "Go Caroling"
 ];
 
-summerList.forEach((item)=> {
-    let div = document.createElement('div');
-    div.innerHTML = item;
-    div.classList = 'listItems'
-    document.getElementById('newItems').appendChild(div);
-})
+const winterList = [
+    "Make Paper Snowflakes",
+    "Make a Snowman",
+    "Sip Hot Chocolate",
+    "Go See a Movie"
+];
+
+const springList = [
+    "Plant Flowers/Veg",
+    "Play in the Rain",
+    "Birding Day",
+    "Kayaking"
+];
+
+const summerList = [
+    "Swimming in Lake",
+    "Go Fishing",
+    "Pick Summer Produce",
+    "Make Homemade Lemonade"
+];
+
+const fallList = [
+    "Hay Ride",
+    "Make Apple Cider",
+    "Watch a Scary Movie",
+    "U-Pick Pumpkins"
+];
+
+function addListItems(season, list, seasonClass) {
+    if (selection.value === season) {
+        list.forEach((item)=> {
+            let div = document.createElement('div');
+            div.innerHTML = item;
+            div.classList = seasonClass;
+            document.getElementById('newItems').appendChild(div);
+        });
+    }
+}
+
+selection.addEventListener('change', (event) => {
+    document.getElementById('newItems').innerHTML = '';
+    addListItems('xmas', xmasList, 'xmasClass');
+    addListItems('winter', winterList, 'winterClass');
+    addListItems('spring', springList, 'springClass');
+    addListItems('summer', summerList, 'summerClass');
+    addListItems('fall', fallList, 'winterClass');
+});
+
